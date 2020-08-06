9773c5c168a0a953741b59511193873ece2524bf
huegogh <huegogh42@gmail.com>
2020-08-05 19:34:42 -0700

    add project files

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Huegogh's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
+            var stats = book.GetStatistics();
+
+            System.Console.WriteLine($"The lowest grade is {stats.Low}");
+            System.Console.WriteLine($"The highest grade is {stats.High}");
+            System.Console.WriteLine($"The average grade is {stats.Average:N1}");
+        }
+    }
+}
